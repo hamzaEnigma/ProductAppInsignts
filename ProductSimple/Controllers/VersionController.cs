db0a1967b19989b600a365f10f5604b6ca4a1796
somai hamza <HAMZASOMAI@outlook.com>
2026-05-14 11:42:56 +0200

    feat: add /api/version controller with auto build date

---
+++ ProductSimple/Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductSimple.Infrastructure;
+
+namespace ProductSimple.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get() 
+        {
+            var result = new
+            {
+                Version = "1.0.0",
+                BuiltAt = BuildInfo.builtAt,
+                Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ ProductSimple/Infrastructure/BuildInfo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ProductSimple.Infrastructure
+{
+    public static class BuildInfo
+    {
+        public static readonly DateTime builtAt = GetBuildDate();
+
+        private static DateTime GetBuildDate()
+        {
+            var filePath = typeof(BuildInfo).Assembly.Location;
+
+            if (File.Exists(filePath))
+                return File.GetLastWriteTimeUtc(filePath);
+            else
+                return DateTime.UtcNow;
+
+        }
+    }
+}
